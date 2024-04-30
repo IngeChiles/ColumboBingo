a27daba39a4f3937b25fbb47acb223723960b5f2
Inge Chiles <Inge.Chiles@gmail.com>
2024-04-29 19:16:02 -0500

    Initial Commit

---
+++ ColumboBingo/ColumboBingoApp.swift
@@ -0,0 +1,14 @@
+//  ColumboBingoApp.swift
+//  ColumboBingo
+//  Made with ♥︎ by Inge Chiles on 4/29/24.
+
+import SwiftUI
+
+@main
+struct ColumboBingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
